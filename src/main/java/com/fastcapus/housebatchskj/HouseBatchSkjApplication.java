cc3c325d0b88290686e3ff1b018c386ef5617c92
손광진 <start_so@naver.com>
2022-12-06 14:30:51 +0900

    프로젝트 초기화, 프로젝트 설정(application.yml, dokcer-compose.yml)

---
+++ src/main/java/com/fastcapus/housebatchskj/HouseBatchSkjApplication.java
@@ -0,0 +1,13 @@
+package com.fastcapus.housebatchskj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseBatchSkjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseBatchSkjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcapus/housebatchskj/HouseBatchSkjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcapus.housebatchskj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseBatchSkjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
